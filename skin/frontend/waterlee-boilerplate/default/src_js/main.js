c2f317eb01f8794aabd07f721c3bfa494972d30c
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-10-20 18:52:28 +0200

    Fix how it works's section animators

--- skin/frontend/waterlee-boilerplate/default/src_js/main.js
+++ skin/frontend/waterlee-boilerplate/default/src_js/main.js
@@ -1,12 +1,12 @@
-var tt = 0;
-jQuery(document).ready( function(){
-
+(function($) {
+$(document).ready( function(){
+  var tt = 0;
   // Home page scroll logic
-  jQuery(window).scroll(function(){
-    var a = jQuery("#header-container");
-    var zb = jQuery("#zip-bar");
-    var sl = jQuery(window).height() - 130; //a.css("height");
-    ds = jQuery(document).scrollTop();
+  $(window).scroll(function(){
+    var a = $("#header-container");
+    var zb = $("#zip-bar");
+    var sl = $(window).height() - 130; //a.css("height");
+    ds = $(document).scrollTop();
     // Scroll up
     if (tt > ds ) {
       if (ds < (sl + 56) ){
@@ -25,72 +25,27 @@ jQuery(document).ready( function(){
     }
     tt = ds;
 
-    var cell = jQuery("#first-basket-cell").css("height");
-    jQuery(".resizable").css("height", cell);
+    var cell = $("#first-basket-cell").css("height");
+    $(".resizable").css("height", cell);
     /* For testing zip bar scrolling when height changes */
-    //var spec = jQuery('<span id="pepe-1"></span></br><span id="pepe-2"></span>');
-    //jQuery("#header-fill").append(spec);
-    //jQuery("#pepe-1").html("Document scroll:" + doc_scroll);
-    //jQuery("#pepe-2").html("Zip bar position:" + zip_bar.css("top"));
+    //var spec = $('<span id="pepe-1"></span></br><span id="pepe-2"></span>');
+    //$("#header-fill").append(spec);
+    //$("#pepe-1").html("Document scroll:" + doc_scroll);
+    //$("#pepe-2").html("Zip bar position:" + zip_bar.css("top"));
     /* //For testing zip bar scrolling when height changes */
 
   });
 
-
   /* Calling triggers on init */
-  callAnimations(wrapAnimators(5));
-  jQuery(document).on("ready", fixHeight);
-  jQuery(window).on("resize", fixHeight);
+  $(document).on("ready", fixHeight);
+  $(window).on("resize", fixHeight);
 });
-
-/* Home page "How it works section"'s logic */
-function animatorMouseOver(elem){
-  elem.mouseover(function(){
-    var pos = elem.css("top");
-    if (pos == "0px") {
-      jQuery(this).stop().animate({
-        top: "-=160",
-        background: "transparent",
-      }, 550, function() {
-        // Animation complete.
-        jQuery(this).css("top", "-160px");
-      });
-    }
-  });
-}
-function animatorMouseOut(elem){
-  elem.mouseout(function(){
-    var pos = elem.css("top");
-    if (pos == "-160px") {
-      jQuery(this).stop().animate({
-        top: "+=160",
-      }, 550, function() {
-        // Animation complete.
-        jQuery(this).css("top", "0px");
-      });
-    }
-  });
-}
-function callAnimations(elems){
-  for(i=0;i<elems.length;i++){
-    el = jQuery(elems[i]);
-    animatorMouseOver(el);
-    animatorMouseOut(el);
-  }
-}
-function wrapAnimators(counter){
-  var arr = [];
-  for(i=1;i<=counter;i++){
-    arr.push("#animator-"+i);
-  }
-  return arr;
-}
 
 /* Home page header resizer function */
 
 function fixHeight() {
-  var a = jQuery("#home #header-container"), k = jQuery(".orbit-bullets-container"),
-      b = (jQuery(window).width(), jQuery(window).height());
+  var a = $("#home #header-container"), k = $(".orbit-bullets-container"),
+      b = ($(window).width(), $(window).height());
   a.css({
     "min-height": b + "px",
     "height": b + "px"
@@ -103,8 +58,8 @@ function fixHeight() {
   k.css({
     "top": v + "px"
   });
-  var c, d, e, f = jQuery("#home #header-container .header-content"),
-      g = jQuery("#home #header-container .header-content");
+  var c, d, e, f = $("#home #header-container .header-content"),
+      g = $("#home #header-container .header-content");
   c = f.height(), d = a.height(), e = (d - c) / 2, f.css({
     "margin-top": e + "px"
   }), c = g.height(), d = a.height(), e = (d - c) / 2, g.css({
@@ -112,3 +67,4 @@ function fixHeight() {
   })
 }
 
+})(jQuery);
