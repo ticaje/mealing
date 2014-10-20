d1fb6f43b58e4fe25272614b319da1b52fd9ce0d
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-10-20 13:50:18 +0200

    Update counter on basket page

--- skin/frontend/waterlee-boilerplate/default/src_js/main.js
+++ skin/frontend/waterlee-boilerplate/default/src_js/main.js
@@ -1,142 +1,114 @@
-// PNG Fallback for SVG
-jQuery(document).ready(function() {
-    if (!Modernizr.svg) {
-        jQuery('img[src*="svg"]').attr('src', function() {
-            return jQuery(this).attr('src').replace('.svg', '.png');
-        });
+var tt = 0;
+jQuery(document).ready( function(){
+
+  // Home page scroll logic
+  jQuery(window).scroll(function(){
+    var a = jQuery("#header-container");
+    var zb = jQuery("#zip-bar");
+    var sl = jQuery(window).height() - 130; //a.css("height");
+    ds = jQuery(document).scrollTop();
+    // Scroll up
+    if (tt > ds ) {
+      if (ds < (sl + 56) ){
+        var u = sl - ds + 56;
+        zb.css( {"top" : u+"px", "position" : "fixed", "width":"100%"} );
+      }
     }
+    // Scroll down
+    else if (tt < ds ){
+      if (ds > (sl + 55) ){
+        zb.css( {"top" : "0", "position" : "fixed", "width":"100%"} );
+      }else{
+        var u = -120; // Set to zip bar's initial position
+        zb.css( {"top" : u+"px", "position" : "relative", "width":"100%"} );
+      }
+    }
+    tt = ds;
+
+    var cell = jQuery("#first-basket-cell").css("height");
+    jQuery(".resizable").css("height", cell);
+    /* For testing zip bar scrolling when height changes */
+    //var spec = jQuery('<span id="pepe-1"></span></br><span id="pepe-2"></span>');
+    //jQuery("#header-fill").append(spec);
+    //jQuery("#pepe-1").html("Document scroll:" + doc_scroll);
+    //jQuery("#pepe-2").html("Zip bar position:" + zip_bar.css("top"));
+    /* //For testing zip bar scrolling when height changes */
+
+  });
+
+
+  /* Calling triggers on init */
+  callAnimations(wrapAnimators(5));
+  jQuery(document).on("ready", fixHeight);
+  jQuery(window).on("resize", fixHeight);
 });
 
-// Legal notice
-jQuery(function() {
-    jQuery(".link_legal-notice").click(function() {
-        jQuery("#legal-notice-wrapper").slideToggle();
-        jQuery("html, body").animate({
-            scrollTop: jQuery("#legal-notice-wrapper").offset().top
-                // need only when navigation is fixed
-                //scrollTop: jQuery("#unternehmen").offset().top -navHeight
-        }, 1000);
-        return false;
-    });
-
-    jQuery("#legal-notice-wrapper .btn-close").click(function() {
-        jQuery("#legal-notice-wrapper").slideUp();
-        return false;
-    });
-});
-
-//Fallback for Internet Explorer
-if (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0) {
-    jQuery('html').addClass('internet-explorer');
+/* Home page "How it works section"'s logic */
+function animatorMouseOver(elem){
+  elem.mouseover(function(){
+    var pos = elem.css("top");
+    if (pos == "0px") {
+      jQuery(this).stop().animate({
+        top: "-=160",
+        background: "transparent",
+      }, 550, function() {
+        // Animation complete.
+        jQuery(this).css("top", "-160px");
+      });
+    }
+  });
+}
+function animatorMouseOut(elem){
+  elem.mouseout(function(){
+    var pos = elem.css("top");
+    if (pos == "-160px") {
+      jQuery(this).stop().animate({
+        top: "+=160",
+      }, 550, function() {
+        // Animation complete.
+        jQuery(this).css("top", "0px");
+      });
+    }
+  });
+}
+function callAnimations(elems){
+  for(i=0;i<elems.length;i++){
+    el = jQuery(elems[i]);
+    animatorMouseOver(el);
+    animatorMouseOut(el);
+  }
+}
+function wrapAnimators(counter){
+  var arr = [];
+  for(i=1;i<=counter;i++){
+    arr.push("#animator-"+i);
+  }
+  return arr;
 }
 
-//Flip
-jQuery(document).ready(function() {
-    var currentDate = new Date();
-    var jQuerycountdown = jQuery('.counter-container .countdown');
-    var firstCalculation = true;
-    jQuerycountdown.countDown({
-        day: 1,
-        month: 1,
-        year: 2015,
-        leftHandZeros: true,
-        dayLabel: null,
-        hourLabel: null,
-        minuteLabel: null,
-        secondLabel: null,
-        afterCalculation: function() {
-            var plugin = this,
-                units = {
-                    days: this.days,
-                    hours: this.hours,
-                    minutes: this.minutes,
-                    seconds: this.seconds
-                },
-                //max values per unit
-                maxValues = {
-                    hours: '23',
-                    minutes: '59',
-                    seconds: '59'
-                },
-                actClass = 'active',
-                befClass = 'before';
+/* Home page header resizer function */
 
-            //build necessary elements
-            if (firstCalculation == true) {
-                firstCalculation = false;
+function fixHeight() {
+  var a = jQuery("#home #header-container"), k = jQuery(".orbit-bullets-container"),
+      b = (jQuery(window).width(), jQuery(window).height());
+  a.css({
+    "min-height": b + "px",
+    "height": b + "px"
+  });
 
-                //build necessary markup
-                jQuerycountdown.find('.unit-wrap div').each(function() {
-                    var jQuerythis = jQuery(this),
-                        className = jQuerythis.attr('class'),
-                        value = units[className],
-                        sub = '',
-                        dig = '';
+  var v = -140;
+  if (b <= 600){
+    v =  v + (600 - b);
+  }
+  k.css({
+    "top": v + "px"
+  });
+  var c, d, e, f = jQuery("#home #header-container .header-content"),
+      g = jQuery("#home #header-container .header-content");
+  c = f.height(), d = a.height(), e = (d - c) / 2, f.css({
+    "margin-top": e + "px"
+  }), c = g.height(), d = a.height(), e = (d - c) / 2, g.css({
+    "margin-top": e + "px"
+  })
+}
 
-                    //build markup per unit digit
-                    for (var x = 0; x < 10; x++) {
-                        sub += [
-                            '<div class="digits-inner">',
-                            '<div class="effect-wrap">',
-                            '<div class="up">',
-                            '<div class="shadow"></div>',
-                            '<div class="inn">' + x + '</div>',
-                            '</div>',
-                            '<div class="down">',
-                            '<div class="shadow"></div>',
-                            '<div class="inn">' + x + '</div>',
-                            '</div>',
-                            '</div>',
-                            '</div>'
-                        ].join('');
-                    }
-
-                    //build markup for number
-                    //for (var i = 0; i < value.length; i++) {
-                    //  dig += '<div class="digits">' + sub + '</div>';
-                    //}
-                    //jQuerythis.append(dig);
-                });
-            }
-
-            //iterate through units
-            jQuery.each(units, function(unit) {
-                var digitCount = jQuerycountdown.find('.' + unit + ' .digits').length,
-                    maxValueUnit = maxValues[unit],
-                    maxValueDigit,
-                    value = plugin.strPad(this, digitCount, '0');
-
-                //iterate through digits of an unit
-                for (var i = value.length - 1; i >= 0; i--) {
-                    var jQuerydigitsWrap = jQuerycountdown.find('.' + unit + ' .digits:eq(' + (i) + ')'),
-                        jQuerydigits = jQuerydigitsWrap.find('div.digits-inner');
-
-                    //use defined max value for digit or simply 9
-                    if (maxValueUnit) {
-                        maxValueDigit = (maxValueUnit[i] == 0) ? 9 : maxValueUnit[i];
-                    } else {
-                        maxValueDigit = 9;
-                    }
-
-                    //which numbers get the active and before class
-                    var activeIndex = parseInt(value[i]),
-                        beforeIndex = (activeIndex == maxValueDigit) ? 0 : activeIndex + 1;
-
-                    //check if value change is needed
-                    if (jQuerydigits.eq(beforeIndex).hasClass(actClass)) {
-                        jQuerydigits.parent().addClass('play');
-                    }
-
-                    //remove all classes
-                    jQuerydigits
-                        .removeClass(actClass)
-                        .removeClass(befClass);
-
-                    //set classes
-                    jQuerydigits.eq(activeIndex).addClass(actClass);
-                    jQuerydigits.eq(beforeIndex).addClass(befClass);
-                }
-            });
-        }
-    });
-});
