68aa76180f639f9c163e29f9cd5338ed1a96c4db
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-11-02 22:02:21 +0100

    Add relationships model declaration

---
+++ app/code/local/Recomiendo/Recipes/Model/Relation/Ingredienttype/Ingredient.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Ingredienttype_Ingredient relationship model
+ *
+ * @author Hector Luis Barrientos Margolles
+ */
+class Recomiendo_Recipes_Model_Relation_Ingredienttype_Ingredient extends Mage_Core_Model_Abstract
+{
+    /**
+     * Define resource model
+     */
+    protected function _construct()
+    {
+        $this->_init('recomiendo_recipes/relation_ingredienttype_ingredient');
+    }
+
+}
---
+++ app/code/local/Recomiendo/Recipes/Model/Relation/Recipe/Util.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Recipe_Util relationship model
+ *
+ * @author Hector Luis Barrientos Margolles
+ */
+class Recomiendo_Recipes_Model_Relation_Recipe_Util extends Mage_Core_Model_Abstract
+{
+    /**
+     * Define resource model
+     */
+    protected function _construct()
+    {
+        $this->_init('recomiendo_recipes/relation_recipe_util');
+    }
+
+}
