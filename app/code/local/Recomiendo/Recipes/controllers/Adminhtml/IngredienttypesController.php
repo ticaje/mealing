7f866fe2dc13ec7936b3afde72bf683dec097504
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-11-04 16:07:31 +0100

    Refactorize codifiers controllers

--- app/code/local/Recomiendo/Recipes/controllers/Adminhtml/IngredienttypesController.php
+++ app/code/local/Recomiendo/Recipes/controllers/Adminhtml/IngredienttypesController.php
@@ -5,247 +5,56 @@
  * @author Hector Luis Barrientos Margolles
  *
  */
-class Recomiendo_Recipes_Adminhtml_IngredienttypesController extends Mage_Adminhtml_Controller_Action
+class Recomiendo_Recipes_Adminhtml_IngredienttypesController extends Recomiendo_Recipes_Controller_Adminhtml_Codifier_BaseController
 {
 
+  /* Define general variables based on refactoring */
+  protected $_currentEntity          = "Tipos de Ingredientes";
+  protected $_currentEntityModelName = "recomiendo_recipes/codifier_ingredienttype";
+  protected $_titleLabel             = "Tipo Ingrediente";
+  protected $_registeringEntityName  = "ingredienttypes_item";
+  protected $_getParamId             = "ingredienttype_id";
+
   /**
-   * Init actions
-   *
-   * @return Recomiendo_Ingredienttypes_Adminhtml_IngredienttypesController
+   * Index action defined in parent, redefine it here if necesary
    */
-  protected function _initAction()
-  {
-    // load layout, set active menu and breadcrumbs
-    $this->loadLayout()
-      ->_setActiveMenu('ingredienttypes/manage')
-      ->_addBreadcrumb(
-        Mage::helper('recomiendo_recipes')->__('Ingredienttypes'),
-        Mage::helper('recomiendo_recipes')->__('Ingredienttypes')
-      )
-      ->_addBreadcrumb(
-        Mage::helper('recomiendo_recipes')->__('Manage Ingredienttypes'),
-        Mage::helper('recomiendo_recipes')->__('Manage Ingredienttypes')
-      )
-      ;
-    return $this;
-  }
 
   /**
-   * Index action
+   * Create New Ingredienttypes item, defined in parent, redefine it here if necesary
    */
-  public function indexAction()
-  {
-    $this->_title($this->__('Ingredienttypes'))
-      ->_title($this->__('Manage Ingredienttypes'));
-
-    $this->_initAction();
-    $this->renderLayout();
-  }
 
   /**
-   * Create new Ingredienttypes item
+   * Edit Ingredienttypes item, defined in parent, redefine it here if necesary
    */
-  public function newAction()
-  {
-    // the same form is used to create and edit
-    $this->_forward('edit');
-  }
 
   /**
-   * Edit Ingredienttypes item
+   * Save action, defined in parent, redefine it here if necesary
    */
-  public function editAction()
-  {
-    $this->_title($this->__('Ingredienttypes'))
-      ->_title($this->__('Manage Ingredienttypes'));
-
-    // 1. instance recipes model
-    /* @var $model Recomiendo_Ingredienttypes_Model_Item */
-    $model = Mage::getModel('recomiendo_recipes/codifier_ingredienttype');
-
-    // 2. if exists id, check it and load data
-    $ingredienttypeId = $this->getRequest()->getParam('id');
-    if ($ingredienttypeId) {
-      $model->load($ingredienttypeId);
-
-      if (!$model->getId()) {
-        $this->_getSession()->addError(
-          Mage::helper('recomiendo_recipes')->__('Ingredienttypes item does not exist.')
-        );
-        return $this->_redirect('*/*/');
-      }
-      // prepare title
-      $this->_title($model->getTitle());
-      $breadCrumb = Mage::helper('recomiendo_recipes')->__('Edit Item');
-    } else {
-      $this->_title(Mage::helper('recomiendo_recipes')->__('New Item'));
-      $breadCrumb = Mage::helper('recomiendo_recipes')->__('New Item');
-    }
-
-    // Init breadcrumbs
-    $this->_initAction()->_addBreadcrumb($breadCrumb, $breadCrumb);
-
-    // 3. Set entered data if was error when we do save
-    $data = Mage::getSingleton('adminhtml/session')->getFormData(true);
-    if (!empty($data)) {
-      $model->addData($data);
-    }
-
-    // 4. Register model to use later in blocks
-    Mage::register('ingredienttypes_item', $model);
-
-    // 5. render layout
-    $this->renderLayout();
-  }
 
   /**
-   * Save action
+   * Delete action, defined in parent, redefine it here if necesary
    */
-  public function saveAction()
-  {
-    $redirectPath   = '*/*';
-    $redirectParams = array();
-
-    // check if data sent
-    $data = $this->getRequest()->getPost();
-    if ($data) {
-      $data = $this->_filterPostData($data);
-      // init model and set data
-      /* @var $model Recomiendo_Ingredienttypes_Model_Item */
-      $model = Mage::getModel('recomiendo_recipes/codifier_ingredienttype');
-
-      // if recipes item exists, try to load it
-      $ingredienttypeId = $this->getRequest()->getParam('ingredienttype_id');
-      if ($ingredienttypeId) {
-        $model->load($ingredienttypeId);
-      }
-
-      $model->addData($data);
-
-      try {
-        $hasError = false;
-        // save the data
-        $model->save();
-
-        // display success message
-        $this->_getSession()->addSuccess(
-          Mage::helper('recomiendo_recipes')->__('The ingredienttype item has been saved.')
-        );
-
-        // check if 'Save and Continue'
-        if ($this->getRequest()->getParam('back')) {
-          $redirectPath   = '*/*/edit';
-          $redirectParams = array('id' => $model->getId());
-        }
-      } catch (Mage_Core_Exception $e) {
-        $hasError = true;
-        $this->_getSession()->addError($e->getMessage());
-      } catch (Exception $e) {
-        $hasError = true;
-        $this->_getSession()->addException($e,
-          Mage::helper('recomiendo_recipes')->__('An error occurred while saving the ingredienttype item.')
-        );
-      }
-
-      if ($hasError) {
-        $this->_getSession()->setFormData($data);
-        $redirectPath   = '*/*/edit';
-        $redirectParams = array('id' => $this->getRequest()->getParam('id'));
-      }
-    }
-
-    $this->_redirect($redirectPath, $redirectParams);
-  }
 
   /**
-   * Delete action
-   */
-  public function deleteAction()
-  {
-    // check if we know what should be deleted
-    $itemId = $this->getRequest()->getParam('id');
-    if ($itemId) {
-      try {
-        // init model and delete
-        /** @var $model Recomiendo_Ingredienttypes_Model_Item */
-        $model = Mage::getModel('recomiendo_recipes/codifier_ingredienttype');
-        $model->load($itemId);
-        if (!$model->getId()) {
-          Mage::throwException(Mage::helper('recomiendo_recipes')->__('Unable to find a recipes item.'));
-        }
-        $model->delete();
-
-        // display success message
-        $this->_getSession()->addSuccess(
-          Mage::helper('recomiendo_recipes')->__('The ingredient type item has been deleted.')
-        );
-      } catch (Mage_Core_Exception $e) {
-        $this->_getSession()->addError($e->getMessage());
-      } catch (Exception $e) {
-        $this->_getSession()->addException($e,
-          Mage::helper('recomiendo_recipes')->__('An error occurred while deleting the recipes item.')
-        );
-      }
-    }
-
-    // go to grid
-    $this->_redirect('*/*/');
-  }
-
-  /**
-   * Check the permission to run it
+   * Check the permission to run it, defined in parent, redefine it here if necesary
    *
    * @return boolean
    */
-  protected function _isAllowed()
-  {
-    switch ($this->getRequest()->getActionName()) {
-    case 'new':
-    case 'save':
-      return Mage::getSingleton('admin/session')->isAllowed('recipes/manage/save');
-      break;
-    case 'delete':
-      return Mage::getSingleton('admin/session')->isAllowed('recipes/manage/delete');
-      break;
-    default:
-      return Mage::getSingleton('admin/session')->isAllowed('recipes/manage');
-      break;
-    }
-  }
 
   /**
-   * Filtering posted data. Converting localized data if needed
+   * Filtering posted data. Converting localized data if needed, defined in parent, redefine it here if necesary
    *
    * @param array
    * @return array
    */
-  protected function _filterPostData($data)
-  {
-    $data = $this->_filterDates($data, array('time_published'));
-    return $data;
-  }
 
   /**
-   * Grid ajax action
+   * Grid ajax action, defined in parent, redefine it here if necesary
    */
-  public function gridAction()
-  {
-    $this->loadLayout();
-    $this->renderLayout();
-  }
 
   /**
-   * Flush Ingredienttypes Posts Images Cache action
+   * Flush Ingredienttypes Posts Images Cache action, defined in parent, redefine it here if necesary
    */
-  public function flushAction()
-  {
-    if (Mage::helper('recomiendo_recipes/image')->flushImagesCache()) {
-      $this->_getSession()->addSuccess('Cache successfully flushed');
-    } else {
-      $this->_getSession()->addError('There was error during flushing cache');
-    }
-    $this->_forward('index');
-  }
 }
 
 ?>
--- app/code/local/Recomiendo/Recipes/controllers/Adminhtml/RecipesController.php
+++ app/code/local/Recomiendo/Recipes/controllers/Adminhtml/RecipesController.php
@@ -5,270 +5,56 @@
  * @author Hector Luis Barrientos Margolles
  *
  */
-class Recomiendo_Recipes_Adminhtml_RecipesController extends Mage_Adminhtml_Controller_Action
+class Recomiendo_Recipes_Adminhtml_RecipesController extends Recomiendo_Recipes_Controller_Adminhtml_Codifier_BaseController
 {
 
+  /* Define general variables based on refactoring */
+  protected $_currentEntity          = "Recetas";
+  protected $_currentEntityModelName = "recomiendo_recipes/recipe";
+  protected $_titleLabel             = "Rectas";
+  protected $_registeringEntityName  = "recipes_item";
+  protected $_getParamId             = "recipe_id";
+
   /**
-   * Init actions
-   *
-   * @return Recomiendo_Recipes_Adminhtml_RecipesController
+   * Index action defined in parent, redefine it here if necesary
    */
-  protected function _initAction()
-  {
-    // load layout, set active menu and breadcrumbs
-    $this->loadLayout()
-      ->_setActiveMenu('recipes/manage')
-      ->_addBreadcrumb(
-        Mage::helper('recomiendo_recipes')->__('Recipes'),
-        Mage::helper('recomiendo_recipes')->__('Recipes')
-      )
-      ->_addBreadcrumb(
-        Mage::helper('recomiendo_recipes')->__('Manage Recipes'),
-        Mage::helper('recomiendo_recipes')->__('Manage Recipes')
-      )
-      ;
-    return $this;
-  }
 
   /**
-   * Index action
+   * Create New Ingredienttypes item, defined in parent, redefine it here if necesary
    */
-  public function indexAction()
-  {
-    $this->_title($this->__('Recipes'))
-      ->_title($this->__('Manage Recipes'));
-
-    $this->_initAction();
-    $this->renderLayout();
-  }
 
   /**
-   * Create new Recipes item
+   * Edit Ingredienttypes item, defined in parent, redefine it here if necesary
    */
-  public function newAction()
-  {
-    // the same form is used to create and edit
-    $this->_forward('edit');
-  }
 
   /**
-   * Edit Recipes item
+   * Save action, defined in parent, redefine it here if necesary
    */
-  public function editAction()
-  {
-    $this->_title($this->__('Recipes'))
-      ->_title($this->__('Manage Recipes'));
-
-    // 1. instance recipes model
-    /* @var $model Recomiendo_Recipes_Model_Item */
-    $model = Mage::getModel('recomiendo_recipes/recipe');
-
-    // 2. if exists id, check it and load data
-    $recipeId = $this->getRequest()->getParam('id');
-    if ($recipeId) {
-      $model->load($recipeId);
-
-      if (!$model->getId()) {
-        $this->_getSession()->addError(
-          Mage::helper('recomiendo_recipes')->__('Recipes item does not exist.')
-        );
-        return $this->_redirect('*/*/');
-      }
-      // prepare title
-      $this->_title($model->getTitle());
-      $breadCrumb = Mage::helper('recomiendo_recipes')->__('Edit Item');
-    } else {
-      $this->_title(Mage::helper('recomiendo_recipes')->__('New Item'));
-      $breadCrumb = Mage::helper('recomiendo_recipes')->__('New Item');
-    }
-
-    // Init breadcrumbs
-    $this->_initAction()->_addBreadcrumb($breadCrumb, $breadCrumb);
-
-    // 3. Set entered data if was error when we do save
-    $data = Mage::getSingleton('adminhtml/session')->getFormData(true);
-    if (!empty($data)) {
-      $model->addData($data);
-    }
-
-    // 4. Register model to use later in blocks
-    Mage::register('recipes_item', $model);
-
-    // 5. render layout
-    $this->renderLayout();
-  }
 
   /**
-   * Save action
+   * Delete action, defined in parent, redefine it here if necesary
    */
-  public function saveAction()
-  {
-    $redirectPath   = '*/*';
-    $redirectParams = array();
-
-    // check if data sent
-    $data = $this->getRequest()->getPost();
-    if ($data) {
-      $data = $this->_filterPostData($data);
-      // init model and set data
-      /* @var $model Recomiendo_Recipes_Model_Item */
-      $model = Mage::getModel('recomiendo_recipes/recipe');
-
-      // if recipes item exists, try to load it
-      $recipeId = $this->getRequest()->getParam('recipe_id');
-      if ($recipeId) {
-        $model->load($recipeId);
-      }
-      // save image data and remove from data array
-      if (isset($data['image'])) {
-        $imageData = $data['image'];
-        unset($data['image']);
-      } else {
-        $imageData = array();
-      }
-      $model->addData($data);
-
-      try {
-        $hasError = false;
-        /* @var $imageHelper Recomiendo_Recipes_Helper_Image */
-        $imageHelper = Mage::helper('recomiendo_recipes/image');
-        // remove image
-
-        if (isset($imageData['delete']) && $model->getImage()) {
-          $imageHelper->removeImage($model->getImage());
-          $model->setImage(null);
-        }
-
-        // upload new image
-        $imageFile = $imageHelper->uploadImage('image');
-        if ($imageFile) {
-          if ($model->getImage()) {
-            $imageHelper->removeImage($model->getImage());
-          }
-          $model->setImage($imageFile);
-        }
-        // save the data
-        $model->save();
-
-        // display success message
-        $this->_getSession()->addSuccess(
-          Mage::helper('recomiendo_recipes')->__('The recipes item has been saved.')
-        );
-
-        // check if 'Save and Continue'
-        if ($this->getRequest()->getParam('back')) {
-          $redirectPath   = '*/*/edit';
-          $redirectParams = array('id' => $model->getId());
-        }
-      } catch (Mage_Core_Exception $e) {
-        $hasError = true;
-        $this->_getSession()->addError($e->getMessage());
-      } catch (Exception $e) {
-        $hasError = true;
-        $this->_getSession()->addException($e,
-          Mage::helper('recomiendo_recipes')->__('An error occurred while saving the recipes item.')
-        );
-      }
-
-      if ($hasError) {
-        $this->_getSession()->setFormData($data);
-        $redirectPath   = '*/*/edit';
-        $redirectParams = array('id' => $this->getRequest()->getParam('id'));
-      }
-    }
-
-    $this->_redirect($redirectPath, $redirectParams);
-  }
 
   /**
-   * Delete action
-   */
-  public function deleteAction()
-  {
-    // check if we know what should be deleted
-    $itemId = $this->getRequest()->getParam('id');
-    if ($itemId) {
-      try {
-        // init model and delete
-        /** @var $model Recomiendo_Recipes_Model_Item */
-        $model = Mage::getModel('recomiendo_recipes/recipe');
-        $model->load($itemId);
-        if (!$model->getId()) {
-          Mage::throwException(Mage::helper('recomiendo_recipes')->__('Unable to find a recipes item.'));
-        }
-        $model->delete();
-
-        // display success message
-        $this->_getSession()->addSuccess(
-          Mage::helper('recomiendo_recipes')->__('The recipes item has been deleted.')
-        );
-      } catch (Mage_Core_Exception $e) {
-        $this->_getSession()->addError($e->getMessage());
-      } catch (Exception $e) {
-        $this->_getSession()->addException($e,
-          Mage::helper('recomiendo_recipes')->__('An error occurred while deleting the recipes item.')
-        );
-      }
-    }
-
-    // go to grid
-    $this->_redirect('*/*/');
-  }
-
-  /**
-   * Check the permission to run it
+   * Check the permission to run it, defined in parent, redefine it here if necesary
    *
    * @return boolean
    */
-  protected function _isAllowed()
-  {
-    switch ($this->getRequest()->getActionName()) {
-    case 'new':
-    case 'save':
-      return Mage::getSingleton('admin/session')->isAllowed('recipes/manage/save');
-      break;
-    case 'delete':
-      return Mage::getSingleton('admin/session')->isAllowed('recipes/manage/delete');
-      break;
-    default:
-      return Mage::getSingleton('admin/session')->isAllowed('recipes/manage');
-      break;
-    }
-  }
 
   /**
-   * Filtering posted data. Converting localized data if needed
+   * Filtering posted data. Converting localized data if needed, defined in parent, redefine it here if necesary
    *
    * @param array
    * @return array
    */
-  protected function _filterPostData($data)
-  {
-    $data = $this->_filterDates($data, array('time_published'));
-    return $data;
-  }
 
   /**
-   * Grid ajax action
+   * Grid ajax action, defined in parent, redefine it here if necesary
    */
-  public function gridAction()
-  {
-    $this->loadLayout();
-    $this->renderLayout();
-  }
 
   /**
-   * Flush Recipes Posts Images Cache action
+   * Flush Ingredienttypes Posts Images Cache action, defined in parent, redefine it here if necesary
    */
-  public function flushAction()
-  {
-    if (Mage::helper('recomiendo_recipes/image')->flushImagesCache()) {
-      $this->_getSession()->addSuccess('Cache successfully flushed');
-    } else {
-      $this->_getSession()->addError('There was error during flushing cache');
-    }
-    $this->_forward('index');
-  }
 }
 
 ?>
