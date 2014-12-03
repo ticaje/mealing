bf8faa6c0bae19e8f23eb55edc7465fba7dc8422
Hector Luis Barrientos Margolles <ticaje@qvitta.net>
2014-12-03 20:42:30 +0100

    Remove unnecesary model called Rclassifications

--- app/code/local/Recomiendo/Recipes/Block/Adminhtml/Rclassifications/Edit/Tabs.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Recipe Classifications List admin edit form tabs block
- *
- * @author Hector Luis Barrientos Margolles
- */
-class Recomiendo_Recipes_Block_Adminhtml_Rclassifications_Edit_Tabs extends Recomiendo_Recipes_Block_Adminhtml_Refactor_Edit_BaseTabs
-{
-}
